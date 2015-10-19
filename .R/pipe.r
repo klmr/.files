69a9ef23dcd06f517e70054507c24381a3f9f810
Konrad Rudolph <konrad.rudolph@gmail.com>
2015-10-19 13:03:50 +0100

    Update R pipe operator to magrittr

--- .R/pipe.r
+++ .R/pipe.r
@@ -1,9 +1,1 @@
-# Pipe operator modified after Robert Sugar, e.g. at
-# <http://markmail.org/thread/uygwsdulfvxlydlh>
-`%|%` <- function (x, y) {
-    thecall <- match.call()
-    if (is.name(thecall$y) || is.function(thecall$y))
-        y(x)
-    else
-        eval(thecall$y, list(value = eval(thecall$x)))
-}
+`%>%` <- magrittr::`%>%`
