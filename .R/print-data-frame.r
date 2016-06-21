76cb7be959040a00ee77110161ac331dffca62db
Konrad Rudolph <konrad.rudolph@gmail.com>
2016-06-21 11:52:50 +0100

    Fix interactive legacy data.frame output in R

---
+++ .R/print-data-frame.r
@@ -0,0 +1,7 @@
+print.data.frame = function (x, ..., digits = NULL, quote = FALSE, right = TRUE,
+                             row.names = TRUE)
+{
+    if (! isTRUE(all.equal(rownames(x), as.character(seq_len(nrow(x))))))
+        x = tibble::rownames_to_column(x)
+    print(dplyr::tbl_df(x))
+}
