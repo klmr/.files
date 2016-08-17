8e9249b9f5b3fcabdd8c836143751a504f5baf37
Konrad Rudolph <konrad.rudolph@gmail.com>
2016-08-17 17:03:48 +0100

    Drastically reduce max.print

--- .R/print.r
+++ .R/print.r
@@ -1,6 +1,7 @@
+options(max.print = 1000L)
+
 print.data.frame = function (x, ..., digits = NULL, quote = FALSE, right = TRUE,
-                             row.names = TRUE)
-{
+                             row.names = TRUE) {
     if (! isTRUE(all.equal(rownames(x), as.character(seq_len(nrow(x))))))
         x = tibble::rownames_to_column(x)
     print(dplyr::tbl_df(x))
