4c8e46c2431adc1402a21de307a1ac6a6934aba8
Konrad Rudolph <konrad.rudolph@gmail.com>
2017-05-12 17:15:44 +0100

    Reorganise R configuration
    Extra config files are now distinguished into interactive and
    non-interactive files. The latter are always loaded.

--- .R/config.r
+++ .R/config.r
@@ -18,26 +18,31 @@ options(
 # All the following is executed in its own environment, which will subsequently
 # be attached to the object search path.
 
+local({
+    # Load individual source files
+    profile_env = new.env()
+    local_r_path = file.path(Sys.getenv('HOME'), '.R')
+    config_path = c(local_r_path,
+                    if (interactive()) file.path(local_r_path, 'interactive'))
+    source_files = list.files(config_path,
+                             pattern = '\\.[rR]$', full.names = TRUE)
+    source_files = source_files[-grep('config\\.[rR]', source_files)]
+
+    for (source_file in source_files)
+        # Ignore errors in individual files.
+        try(eval(parse(source_file, encoding = 'UTF-8'), envir = profile_env))
+
+    attach(profile_env, name = 'rprofile', warn.conflicts = FALSE)
+})
+
 if (interactive()) {
     local({
-        # Load individual source files
-        profile_env = new.env()
-        local_r_path = file.path(Sys.getenv('HOME'), '.R')
-        sourcefiles = list.files(local_r_path, pattern = '\\.[rR]$', full.names = TRUE)
-        sourcefiles = sourcefiles[-grep('config\\.[rR]', sourcefiles)]
-
-        for (sourcefile in sourcefiles)
-            # Ignore errors in individual files.
-            try(eval(parse(sourcefile, encoding = 'UTF-8'), envir = profile_env))
-
-        attach(profile_env, name = 'rprofile', warn.conflicts = FALSE)
-
         # Load `modules` last to make `modules::?` findable.
         packages = c('colorout', 'modules')
         options(defaultPackages = c(getOption('defaultPackages'), packages))
-
-        message(R.version$version.string)
     })
+
+    message(R.version$version.string)
 
     .post_package_loading_hook = function () {
         setOutputColors256(verbose = FALSE)
