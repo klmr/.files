e22af788b6c424b71b187a03c70fada95a00fc89
Konrad Rudolph <konrad.rudolph@gmail.com>
2017-10-16 17:56:40 +0100

    Customise R console highlighting

--- .config/R/init.r
+++ .config/R/init.r
@@ -43,9 +43,22 @@ if (interactive()) {
     message(R.version$version.string)
 
     .post_package_loading_hook = function () {
-        setOutputColors256(verbose = FALSE)
+        setOutputColors256(
+            normal = 202,
+            negnum = 214,
+            zero = 209,
+            number = 184,
+            date = 172,
+            string = 179,
+            const = 208,
+            false = 244,
+            true = 208,
+            infinite = 39,
+            stderror = 33,
+            warn = c(1, 0, 1),
+            error = c(1, 3),
+            verbose = FALSE)
     }
 
-    .Last = function ()
-        try(savehistory(Sys.getenv('R_HISTFILE', '~/.Rhistory')))
+    .Last = function () try(savehistory(Sys.getenv('R_HISTFILE', '~/.Rhistory')))
 }
